db3d6492a29e49ce0b16191c964103578330a5db
alvaroormeno <alvaro@ormeno.org>
2022-08-11 16:33:47 -0400

    initialized express, listening to port complete

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,8 @@
-console.log('Hello World')
+const express = require('express');
+const dotenv = require('dotenv').config()
+const port = 5001
+
+//Initialize express
+const app = express()
+
+app.listen(port, () => console.log(`Server started on port ${port}`))
