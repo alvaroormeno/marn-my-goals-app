0aceab73d9f3f238a28dcf6a035e930c2ceb6e80
alvaroormeno <alvaro@ormeno.org>
2022-08-12 17:43:39 -0400

    created user controllers for registering, authenticating and getting user data, imported controllers to userroutes and declared a route for each

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,11 +1,32 @@
 
 // DESCRIPTION - Register a user
 // ROUTE - POST /api/users
-// ACCESS - Private
+// ACCESS - Public
 const registerUser = (req, res) => {
   res.json({message: 'Register User' })
 }
 
+// DESCRIPTION - Authenticate a user
+// ROUTE - POST /api/users/login
+// ACCESS - Public
+const loginUser = (req, res) => {
+  res.json({message: 'Login User' })
+}
+
+// DESCRIPTION - Get user data
+// ROUTE - GET /api/users/me
+// ACCESS - Public
+const getMe = (req, res) => {
+  res.json({message: 'User data display' })
+}
+
+
+
+
+
+
 module.exports = {
   registerUser,
+  loginUser,
+  getMe,
 }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,9 @@
 const express =  require('express');
 const router = express.Router()
-const { registerUser } = require('../controllers/userController.js')
+const { registerUser, loginUser, getMe } = require('../controllers/userController.js')
 
 router.post('/', registerUser)
+router.post('/login', loginUser)
+router.post('/me', getMe)
 
 module.exports = router
