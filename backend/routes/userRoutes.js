03079ce9b5de4889ed2d6f3d6db1803a3ea9eb36
alvaroormeno <alvaro@ormeno.org>
2022-08-12 19:53:38 -0400

    imported protect middleware to userroutes and protected the getme route by adding the protect function as second argument

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,8 +2,12 @@ const express =  require('express');
 const router = express.Router()
 const { registerUser, loginUser, getMe } = require('../controllers/userController.js')
 
+// We need to protect private routes, for example getMe since it gives us the data of user.
+// For this we use the protect middleware.
+const {protect} = require('../middleware/authMiddleware.js')
+
 router.post('/', registerUser)
 router.post('/login', loginUser)
-router.get('/me', getMe)
+router.get('/me', protect, getMe)
 
 module.exports = router
