20933fb8152ef8dfa0e277922955a235831aa856
alvaroormeno <alvaro@ormeno.org>
2022-08-13 17:17:32 -0400

    Header.jsx - implemented one state for all input forms and desconstruct the state to retrieve the intial state for each input

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,18 @@
 import React from 'react'
+import {useState, useEffect} from 'react'
 
 function Register() {
+
+  // Instead of having a different state for each input field, we only have one state for the whole form. This initial state will carry an object with a property and iniitial value of empty for each field.
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: '',
+  })
+  // To retrieve the property values of formData state we destructure it.
+  const {name, email, password, password2} = formData
+
   return (
     <div>Register</div>
   )
