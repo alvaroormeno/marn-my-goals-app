17b616201de25d9aa19801953ecc790aaa15646b
alvaroormeno <alvaro@ormeno.org>
2022-08-13 16:53:53 -0400

    app.js - imported 3 main pages and basic setup using react-router-dom route and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Dashboard from './pages/Dashboard';
+import Register from './pages/Register';
+import Login from './pages/Login';
+
+
 
 function App() {
+
   return (
-    <div className="App">
-      <h1>My App</h1>
-    </div>
+    <>
+      <Router>
+        <div className='container'>
+          <Routes>
+            <Route path='/' element={<Dashboard/>} />
+            <Route path='/' element={<Login/>} />
+            <Route path='/' element={<Register/>} />
+          </Routes>
+        </div>
+      </Router>
+    </>
   );
 }
 
