951a74f61040dc6feee6d1a1b4ff847631392dee
alvaroormeno <alvaro@ormeno.org>
2022-08-13 17:07:11 -0400

    app.js - imported Heder component and placed it inside to render above routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 import Dashboard from './pages/Dashboard';
 import Register from './pages/Register';
 import Login from './pages/Login';
+import Header from './components/Header';
 
 
 
@@ -11,6 +12,7 @@ function App() {
     <>
       <Router>
         <div className='container'>
+          <Header/>
           <Routes>
             <Route path='/' element={<Dashboard/>} />
             <Route path='/' element={<Login/>} />
