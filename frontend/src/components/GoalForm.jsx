a9696c90c53fba2ec5974b58d464ca7045693240
alvaroormeno <alvaro@ormeno.org>
2022-08-14 00:56:53 -0400

    implemented goalform.jsx

--- frontend/src/components/GoalForm.jsx
+++ frontend/src/components/GoalForm.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {useState} from 'react'
-import {useSelector, useDispatch} from 'react-redux'
+import { useDispatch} from 'react-redux'
 import {createGoal} from '../features/goals/goalSlice.js'
 
 function GoalForm() {
