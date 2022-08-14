c2c8c67c1c07ba31d4938e13609e94297eaf8d4f
alvaroormeno <alvaro@ormeno.org>
2022-08-13 23:40:19 -0400

    implemented dashboard.jsx to navigate out of it when logout

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {useNavigate} from 'react-router-dom'
+import {useSelector} from 'react-redux'
 
 function Dashboard() {
+
+  // initialize useNavigate
+  const navigate = useNavigate()
+
+  // bring user from auth state 
+  const {user} = useSelector((state) => state.auth)
+
+  // When we logout we dont want access to the dashboard therefore we need to navigate out
+  useEffect(() => {
+    if(!user) {
+      navigate('/login')
+    }
+  }, [user, navigate])
+
+
   return (
     <div>Dashboard</div>
   )
