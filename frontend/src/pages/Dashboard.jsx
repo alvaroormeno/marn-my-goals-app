5713004aaf0d4c9bc0f0866543f0f344937b8536
alvaroormeno <alvaro@ormeno.org>
2022-08-14 00:56:31 -0400

    created goalform.jsx

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 import {useNavigate} from 'react-router-dom'
 import {useSelector} from 'react-redux'
+import GoalForm from '../components/GoalForm'
 
 function Dashboard() {
 
@@ -24,6 +25,7 @@ function Dashboard() {
       <h1>Welcome {user && user.name}</h1>
       <p>Goals Dashboard</p>
     </section>
+    <GoalForm/>
     </>
   )
 }
