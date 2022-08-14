43309b7c51630ce05e059558bbd36987224209bd
alvaroormeno <alvaro@ormeno.org>
2022-08-14 00:46:13 -0400

    store.js - added goalreducer

--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -7,3 +7,15 @@ const initialState = {
   isLoading: false,
   message: ''
 }
+
+// create the slice
+export const goalSlice = createSlice({
+  name: 'goal',
+  initialState,
+  reducers: {
+    reset: (state) => initialState
+  }
+})
+
+export const {reset} = goalSlice.actions
+export default goalSlice.reducer
