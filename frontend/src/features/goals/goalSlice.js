a147d95a7099a93bf2152eb4f959ac99227b7253
alvaroormeno <alvaro@ormeno.org>
2022-08-14 01:17:48 -0400

    goalslice.js - created get user goals function

--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -1,5 +1,5 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
-import authService from '../auth/authService'
+import goalService from '../goals/goalService'
 
 const initialState = {
   goals: [],
@@ -23,12 +23,43 @@ export const createGoal = createAsyncThunk('goals/create', async (goalData, thun
   }
 })
 
+// get user goals
+export const getGoals = createAsyncThunk('goals/getAll', async(_, thunkAPI) => {
+  try {
+
+    const token = thunkAPI.getState().auth.user.token
+    return await goalService.getGoals(token)
+    
+  } catch (error) {
+         // save erros to message variable... if any of the following exist then save to const message
+         const message = (error.response && error.response.data & error.response.data.message) || error.message || error.toString()
+         // use thunkapi method rejectWithValue and pass in the message to send the message as payload
+         return thunkAPI.rejectWithValue(message)
+  }
+})
+
 // create the slice
 export const goalSlice = createSlice({
   name: 'goal',
   initialState,
   reducers: {
     reset: (state) => initialState
+  },
+  extraReducers: (builder) => {
+    builder
+    .addCase(createGoal.pending, (state) => {
+      state.isLoading = true
+    })
+    .addCase(createGoal.fulfilled, (state, action) => {
+      state.isLoading = false
+      state.isSuccess = true
+      state.goals.push(action.payload)
+    })
+    .addCase(createGoal.rejected, (state, action) => {
+      state.isLoading = false
+      state.isError = true
+      state.message = action.payload
+    })
   }
 })
 
