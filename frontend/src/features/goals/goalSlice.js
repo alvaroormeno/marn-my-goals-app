f11eb07e1938fa63c87512186af2cef730c64c7f
alvaroormeno <alvaro@ormeno.org>
2022-08-14 01:03:38 -0400

    goalslice.js - created goalthunk function, since they are private used thunkapi to get token

--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -1,4 +1,5 @@
 import {createSlice, createAsyncThunk} from '@reduxjs/toolkit'
+import authService from '../auth/authService'
 
 const initialState = {
   goals: [],
@@ -8,6 +9,20 @@ const initialState = {
   message: ''
 }
 
+//Create new goal thunk function
+export const createGoal = createAsyncThunk('goals/create', async (goalData, thunkAPI) => {
+  try{
+    const token = thunkAPI.getState().auth.user.token
+    return await goalService.createGoal(goalData, token)
+
+  } catch (error) {
+     // save erros to message variable... if any of the following exist then save to const message
+     const message = (error.response && error.response.data & error.response.data.message) || error.message || error.toString()
+     // use thunkapi method rejectWithValue and pass in the message to send the message as payload
+     return thunkAPI.rejectWithValue(message)
+  }
+})
+
 // create the slice
 export const goalSlice = createSlice({
   name: 'goal',
@@ -16,6 +31,8 @@ export const goalSlice = createSlice({
     reset: (state) => initialState
   }
 })
+
+
 
 export const {reset} = goalSlice.actions
 export default goalSlice.reducer
