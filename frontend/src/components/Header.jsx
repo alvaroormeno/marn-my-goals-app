3ed530cc25452567c62a8ef1b8b7b1f994c80f6b
alvaroormeno <alvaro@ormeno.org>
2022-08-13 23:01:32 -0400

    header.jsx - fixed: re insrted login button

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -31,12 +31,17 @@ function Header() {
           </li>
         ) : (
           <>
-            <li>
-              <Link to='/Register'>
-                <FaUser/> Register
-              </Link>
-            </li>
-          </>
+          <li>
+            <Link to='/login'>
+              <FaSignInAlt /> Login
+            </Link>
+          </li>
+          <li>
+            <Link to='/register'>
+              <FaUser /> Register
+            </Link>
+          </li>
+        </>
         )}
         
       </ul>
